62c6716cca4930d753abb7797425355c1482755b
amyly <lyamys@gmail.com>
2013-11-09 15:48:50 -0500

    displays all restaurant checkins in the past 6 days by an autenticated user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-
+    if current_user
+      @user = current_user
+      @user_checkins = current_user.get_checkins(current_user)
+    end
   end
 end
