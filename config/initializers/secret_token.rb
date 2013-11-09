fd1f579677b54924c692ac0b1c90684f992682f1
amyly <lyamys@gmail.com>
2013-11-09 12:43:41 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GirlTalk::Application.config.secret_key_base = '717f26dab419ad4763727ae25c16f0b4695510995fcf01372de1dd6766caa81ded1ae1540c9c875d933a373357b1d5cee5810a5724033ec693dee19a22dfcdce'
