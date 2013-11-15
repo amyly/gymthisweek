cc7b83d61a577dfa945a584249ed0f036be5a588
amyly <lyamys@gmail.com>
2013-11-15 01:10:57 -0500

    auto-lowercases hashtags

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,9 @@
 class Hashtag < ActiveRecord::Base
   belongs_to :user
   validates :hashtag, presence: true
+  before_save :lowercase
+
+  def lowercase
+    self.hashtag = self.hashtag.to_s.downcase
+  end
 end
